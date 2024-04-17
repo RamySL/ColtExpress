a5104f95833a9a1682d92585004a3f057c56e0dd
Ramy <ramy.sail@universite-paris-saclay.fr>
2024-04-17 22:45:16 +0200

    Ecran de fin terminé

--- src/Vue/EcranFin.java
+++ src/Vue/EcranFin.java
@@ -18,65 +18,61 @@ public class EcranFin extends JPanel {
     JButton bouttonRejouer;
     Image imageFond;
     public EcranFin(Fenetre fenetre, Bandit banditGagnant, Map<Personnage, ImageIcon> mapPersonnageIcone){
-        this.imageFond =  new ImageIcon("src/assets/images/jeuBack.jpg").getImage();
+
+        this.imageFond =  new ImageIcon("src/assets/images/FondFin.jpg").getImage();
         this.fenetre = fenetre;
         this.banditGagnant = banditGagnant;
 
-        this.setLayout(new BorderLayout());
+        this.setLayout(null);
+        ImageIcon wantedIcone = new ImageIcon("src/assets/images/wanted.png");
+        // les decalage à faire pour que wanted soit au centre
+        int decalageX = (this.fenetre.getWidth()-wantedIcone.getIconWidth())/2;
+        int decalageY = (this.fenetre.getHeight()-wantedIcone.getIconHeight())/2;
 
-        JPanel northPanel = new JPanel();
-        JPanel eastPanel = new JPanel();
-        JPanel westhPanel = new JPanel();
-        JPanel southhPanel = new JPanel();
 
-        northPanel.setPreferredSize(new Dimension(0,200));
-        southhPanel.setPreferredSize(new Dimension(0,200));
-        eastPanel.setPreferredSize(new Dimension(200,0));
-        westhPanel.setPreferredSize(new Dimension(200,0));
-
-        northPanel.setOpaque(false);
-        eastPanel.setOpaque(false);
-        westhPanel.setOpaque(false);
-        southhPanel.setOpaque(false);
+        JLabel wantedIconeLabel = new JLabel(wantedIcone);
+        wantedIconeLabel.setLayout(null);
 
         JPanel panelCentrale = new JPanel(new BorderLayout());
-        panelCentrale.setBackground(new Color(0xCF000000, true));
-
-         bouttonRejouer = new JButton("Rejouer");
+        //panelCentrale.setBackground(new Color(0xCF000000, true));
+        panelCentrale.setOpaque(false);
+        bouttonRejouer = new JButton("Rejouer");
+        bouttonRejouer.setForeground(Color.WHITE);
+        bouttonRejouer.setBackground(new Color(0xDA523B25, true));
+        bouttonRejouer.setFont(new Font("MV Boli", Font.BOLD, 20));
 
 ////////////////////////////////////// copier coller de panneau de config
         JPanel persoPanel = new JPanel();
-        persoPanel.setBackground(new Color(0xA6000000, true));
-        persoPanel.setLayout(new BoxLayout(persoPanel,BoxLayout.Y_AXIS));
+        //persoPanel.setBackground(new Color(0xA6000000, true));
+        persoPanel.setOpaque(false);
+        persoPanel.setLayout(new BorderLayout());
         JLabel iconeBandit = new JLabel(mapPersonnageIcone.get(banditGagnant));
-        JLabel surnomLabel = new JLabel( "Surnom : " + banditGagnant.getSurnom());
-        JLabel ballesLabel = new JLabel("Balles restantes : " + banditGagnant.getNbBalles());
-        JLabel scoreLabel = new JLabel("Score : " + banditGagnant.score());
+        JLabel surnomLabel = new JLabel( banditGagnant.getSurnom());
+        JLabel scoreLabel = new JLabel(banditGagnant.score() + " €");
+        //iconeBandit.setBorder(new LineBorder(Color.BLUE));
+        surnomLabel.setForeground(Color.black);
+        scoreLabel.setForeground(Color.black);
 
-        surnomLabel.setForeground(Color.WHITE);
-        ballesLabel.setForeground(Color.WHITE);
-        scoreLabel.setForeground(Color.WHITE);
+        surnomLabel.setFont(new Font("MV Boli", Font.BOLD, 20));
+        scoreLabel.setFont(new Font("MV Boli", Font.BOLD, 20));
+        surnomLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        persoPanel.add(iconeBandit,BorderLayout.CENTER);
+        persoPanel.add(surnomLabel, BorderLayout.SOUTH);
 
-        surnomLabel.setFont(new Font("MV Boli", Font.BOLD, 13));
-        ballesLabel.setFont(new Font("MV Boli", Font.BOLD, 13));
-        scoreLabel.setFont(new Font("MV Boli", Font.BOLD, 13));
-
-        persoPanel.add(iconeBandit);
-        persoPanel.add(surnomLabel);
-        persoPanel.add(ballesLabel);
-        persoPanel.add(scoreLabel);
-        persoPanel.setBorder(new LineBorder(Color.BLACK, 1));
+        //persoPanel.setBorder(new LineBorder(Color.BLACK, 1));
         ////////////////////////////////////
 
         panelCentrale.add(persoPanel,BorderLayout.CENTER);
-        panelCentrale.add(bouttonRejouer, BorderLayout.SOUTH);
+        //persoPanel.setBorder(new LineBorder(Color.GREEN,3));
+        scoreLabel.setBounds(131 + 70,538,200,40);
+        panelCentrale.setBounds(88 ,169 ,270,274);
+        wantedIconeLabel.setBounds(decalageX,decalageY,wantedIcone.getIconWidth(),wantedIcone.getIconHeight()-10);
+        bouttonRejouer.setBounds(100,this.fenetre.getHeight()/2,130,35);
+        wantedIconeLabel.add(scoreLabel);
+        wantedIconeLabel.add(panelCentrale);
 
-
-        this.add(panelCentrale, BorderLayout.CENTER);
-        this.add(northPanel, BorderLayout.NORTH);
-        this.add(eastPanel, BorderLayout.EAST);
-        this.add(westhPanel, BorderLayout.WEST);
-        this.add(southhPanel, BorderLayout.SOUTH);
+        this.add(wantedIconeLabel);
+        this.add(bouttonRejouer);
 
     }
 
